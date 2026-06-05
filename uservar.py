2d0e6a08d6325bdf231889c3250b6ca762fdd848
adamsbuilds <61364871+adamsbuilds@users.noreply.github.com>
2026-06-05 12:52:44 -0500

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -1,14 +1,14 @@
 '''#####-----Build File-----#####'''
-buildfile = 'http://CHANGEME'
+buildfile = 'https://raw.githubusercontent.com/adamsbuilds/repo/refs/heads/master/zips/builds.txt'
 
 '''#####-----Videos File-----#####'''
-videos_url = 'http://CHANGEME'
+videos_url = 'https://'
 
 '''#####-----Notification File-----#####'''
-notify_url  = 'http://CHANGEME'
+notify_url  = 'https://raw.githubusercontent.com/adamsbuilds/repo/refs/heads/master/zips/notify.txt'
 
 '''#####-----Changelog Directory-----#####'''
-changelog_dir  = 'http://CHANGEME/'
+changelog_dir  = 'http://'
 
 '''#####-----Excludes-----#####'''
 excludes  = ['plugin.video.whatever']
