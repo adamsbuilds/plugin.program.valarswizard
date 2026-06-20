1b660c8d82a7e0dba29f9f7846baeddfd06743ef
Valar_Morghulis <61364871+adamsbuilds@users.noreply.github.com>
2026-06-19 22:18:54 -0500

    Add files via upload

--- uservar.py
+++ uservar.py
@@ -5,7 +5,7 @@ buildfile = 'https://raw.githubusercontent.com/adamsbuilds/repo/refs/heads/maste
 videos_url = 'http://'
 
 '''#####-----Notification File-----#####'''
-notify_url  = 'https://raw.githubusercontent.com/adamsbuilds/repo/refs/heads/master/zips/valar_notify.txt'
+notify_url  = 'http://'
 
 '''#####-----Changelog Directory-----#####'''
 changelog_dir  = 'http://'
